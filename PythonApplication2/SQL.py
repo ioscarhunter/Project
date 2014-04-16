340871b5130069fe2b0f872e6607c062d05ad927
ioscarhunter <ioscarhunter@gmail.com>
2014-04-16 13:42:20 +0700

    add server thing

--- PythonApplication2/SQL.py
+++ PythonApplication2/SQL.py
@@ -22,10 +22,36 @@ class SQL:
     def __init__ (self):
         self.connect = sqlite3.connect('example.db')
         self.c = self.connect.cursor()
+        self.ordernum=0
+        self.time = int(time.strftime("%d%m%Y"))
         try:
-            self.c.execute('''CREATE TABLE stocks(date text, trans text, symbol text, qty real, price real)''') # Create Table if it not excite
+            self.c.execute("CREATE TABLE stocks(date integer, ordernum integer, size text)") # Create Table if it not excite
         except:
             pass
+            #load ordernum
+
+    def insert(self, data):
+        self.c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', data)
+        self.connect.commit()
+
+    def printrow(self,statement):
+        for row in self.c.execute(statement):
+            print(row)
+
+    def decode(self,text):
+        pass
+        insert()
+
+    def getnumber(self):
+        self.ordernum+=1
+        return self.ordernum
+
 if __name__ == '__main__':
     sl = SQL()
+    purchases = [('20012-03-28', 'BUY', 'Microsoft', 1000, 45.00),
+             ('2011-03-05', 'BUY', 'Bata', 1000, 72.00),
+             ('2007-04-06', 'SELL', 'fdd', 500, 53.00),
+            ]
+    #sl.insert(purchases)
+    sl.printrow('SELECT * FROM stocks ORDER BY date')
     sl.c.close()
---
+++ PythonApplication2/server_Xtion.py
@@ -0,0 +1,59 @@
+import socket
+import sys
+import threading
+ 
+import SQL
+
+PORT = 21567 # the port number to run our server on
+__version__ = "0.0.1"
+ 
+class ChatServer(threading.Thread):
+    
+    def __init__(self, port, host='localhost'):
+        threading.Thread.__init__(self)
+        self.port = port
+        self.host = host
+        self.users = {} # current connections
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+        self.SL = SQL() #SQL
+        
+        try:
+            self.server.bind((self.host, self.port))
+        except socket.error:
+            print('Bind failed %s' % (socket.error))
+            sys.exit()
+ 
+        self.server.listen(10)
+        
+    # Not currently used.  Ensure sockets are closed on disconnect
+    def exit(self):
+        self.server.close()
+ 
+    def run_thread(self, conn, addr):
+        print('Client connected with ' + addr[0] + ':' + str(addr[1]))
+        s = "defrog"
+        while True:
+            try:
+                data = conn.recv(1024)
+                reply = data
+                if (self.SL.decode((reply.decode("utf-8")))): # (bytestring.decode("utf-8") = convert byte string to str
+                    conn.sendall(self.SL.getnumber())
+            except (ConnectionError):
+                print('Connection Error/Close')
+                break
+        conn.close() # Close
+ 
+    def run(self):
+        print('Waiting for connections on port %s' % (self.port))
+        # We need to run a loop and create a new thread for each connection
+        while True:
+            conn, addr = self.server.accept()
+            threading.Thread(target=self.run_thread, args=(conn, addr)).start()
+ 
+  
+if __name__ == '__main__':
+    
+    server = ChatServer(PORT)
+    server.run()
+ 
