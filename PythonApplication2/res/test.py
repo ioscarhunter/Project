38a4165cfe1c7fc0a06b80acdaf0484d5fd57dd4
ioscarhunter <ioscarhunter@gmail.com>
2014-05-20 17:19:27 +0700

    GUI Complete

--- PythonApplication2/res/test.py
+++ PythonApplication2/res/test.py
@@ -1,32 +1,31 @@
-
 import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
-from PySide.QtUiTools import *
+from PySide.QtUiTools import*
+from test2 import menu
 
 class mymainwindow(QMainWindow):
-    def __init__(self):
+    def __init__(self,x):
         QMainWindow.__init__(self)
         loader = QUiLoader()
-
+        x[2] = 9
         form = loader.load("./test.ui", self)
         self.setCentralWidget(form)
         
         self.p1 = form.findChild(QPushButton, "p1")
         p2 = form.findChild(QPushButton, "p2")
         p2.clicked.connect(self.printit)
+        self.m = menu(x)
     def printit(self):
-        if(self.p1.isEnabled()):
-            self.p1.setEnabled(False)
-        else:
-            self.p1.setEnabled(True)
-        print("d")
+        self.m.show()
+        self.close()
 
 def main():
     app = QApplication(sys.argv)
-
-    mywindow = mymainwindow()
+    x = [1,2,3]
+    mywindow = mymainwindow(x)
     mywindow.show()
+    print(x)
     return app.exec_()
 
 if __name__ == "__main__":
